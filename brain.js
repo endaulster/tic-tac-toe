6767f77bace9160ba78c9bd7e2920b9fbada6e97
endaulster <105703311+endaulster@users.noreply.github.com>
2024-01-04 17:47:24 -0300

    Fixed minor bugs

--- brain.js
+++ brain.js
@@ -1,16 +1,18 @@
 function gameShelter () {
 
-    // Cache Dom
-    function cacheDom(){
-        let boxes = Array.from(document.querySelectorAll("box"));
-        boxes.forEach(cul => cul.addEventListener("mousedown", function()
-        {
-            cul.style.backgroundColor="white";
-        }
-        ));
-    }
-    cacheDom();
+
 
 
 }
 
+// Cache Dom
+function cacheDom(){
+    let boxes = Array.from(document.querySelectorAll(".box"));
+    boxes.forEach(cul => cul.addEventListener("mousedown", function()
+    {
+        cul.textContent = "O";
+        
+    }
+    ));
+}
+cacheDom();
